44c9ab541bd95af68b622b3d5fb54da9a88d1335
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-08 15:41:02 +0200

    add bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,33 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nameUser = "Mario";
 
+const today = new Date();
+console.log(today);
+console.log(today.getHours());
 
 // Dichiara la funzione qui.
-
+function greetUser(user) {
+  let helloUser = "";
+  //data odiera
+  const today = new Date();
+  //ora corrente
+  let currentHours = today.getHours();
+  //se l'ora corrente è minore delle 13
+  if (currentHours < 13) {
+    helloUser = "buongiorno";
+    //altrimenti se l'ora corrente è maggiore delle 17
+  } else if (currentHours > 17) {
+    helloUser = "buonasera";
+    //altrimenti se l'ora corrente è compresa tra le 13 e le 15
+  } else {
+    helloUser = "buon pomeriggio";
+  }
+  return `${helloUser}  ${user}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greetUser(nameUser));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
