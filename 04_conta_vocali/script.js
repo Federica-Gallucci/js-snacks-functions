0058eb851b1ae0dce79b900b63c6fe95d5c633bf
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-08 14:45:46 +0200

    add conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,33 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+/**
+ * Funzione che conta il numero di vocali
+ * @param {string} phrase stringa in cui contare le vocali
+ * @returns {number} numero di vocali
+ */
+function countVowels(phrase) {
+  let vowelsNumber = 0;
+  for (let i = 0; i < phrase.length; i++) {
+    const currentChar = phrase[i].toLowerCase();
+    if (
+      currentChar === "a" ||
+      currentChar === "e" ||
+      currentChar === "i" ||
+      currentChar === "o" ||
+      currentChar === "u"
+    )
+      vowelsNumber += 1;
+  }
+  return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const outputText = countVowels(word);
+console.log(outputText);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
